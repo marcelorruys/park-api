2cd07f986bf0909320a758de86f08a3a8bdf327a
marcelorruys <ruysmarcelo@gmail.com>
2024-07-16 20:16:16 -0300

    project created

---
+++ src/test/java/com/marcelo/park_api/ParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.park_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
